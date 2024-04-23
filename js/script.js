3d5fa2ce6c0c78fc18799ce8597ad1c5e971f51c
SistemasCogescorp <113115230+SistemasCogescorp@users.noreply.github.com>
2024-04-23 10:41:34 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,50 @@
+
+//LLAMAR AL ID DEL BOTON
+let mybutton = document.getElementById("btn-back-to-top");
+
+// AL BAJAR 20PX MOSTRAR EL BOTON PARA SUBIR
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// CUANDO SE PRESIONE EL BOTON SUBIR AL INICIO DE LA PAGINA
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// CONTADOR DE ESTADISTICAS
+$('.counter').each(function() {
+  var $this = $(this),
+      countTo = $this.attr('data-count');
+  
+  $({ countNum: $this.text()}).animate({
+    countNum: countTo
+  },
+  {
+    duration: 3000,
+    easing:'linear',
+    step: function() {
+      $this.text(Math.floor(this.countNum));
+    },
+    complete: function() {
+      $this.text(this.countNum);
+      //alert('finished');
+    }
+
+  });  
+  
+});
+
